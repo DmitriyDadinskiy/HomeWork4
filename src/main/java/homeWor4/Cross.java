80c1b015adc556c99575b9f64e97cc2508daaacb
DmitriyDadinskiy <84646255+DmitriyDadinskiy@users.noreply.github.com>
2021-05-28 13:10:55 +1000

    first commit

--- src/main/java/homeWor4/Cross.java
+++ src/main/java/homeWor4/Cross.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 //Полностью разобраться с кодом, попробовать переписать с нуля, стараясь не подглядывать в методичку. (не подглядывать не получилось)
 public class Cross {
 
-    private static final int SIZE = 3;
-    private static final int DOTS_TO_WIN = 3;
+    private static final int SIZE = 5;
+    private static final int DOTS_TO_WIN = 4;
     private static final char DOT_EMPTY = '-';
     private static final char DOT_X = 'x';
     private static final char DOT_0 = '0';
@@ -40,9 +40,9 @@ public class Cross {
                 break;
             }
         }
-                System.out.println("Игра окончена");
+        System.out.println("Игра окончена");
 
-        }
+    }
 
 
     private static void initMap() {
@@ -59,10 +59,10 @@ public class Cross {
             System.out.print(i + " ");
         }
         System.out.println();
-            for (int i = 0; i < SIZE; i++) {
-                System.out.print(i + 1 + " ");
-                for (int j = 0; j < SIZE; j++) {
-                System.out.print(map[i][j]+" ");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
@@ -80,20 +80,50 @@ public class Cross {
         map[y][x] = DOT_X;
     }
 
-    private static boolean isCellInvalid(int x, int y) пш{
+    private static boolean isCellInvalid(int x, int y) {
         return (x < 0 || x >= SIZE)
                 || (y < 0 || y >= SIZE)
                 || map[y][x] != DOT_EMPTY;
     }
-//    Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов.
+
+    //    Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов.
+    // сначала нужно искать повтороения элементов из DOTS_TO_WIN = 3 подряд  в колонке, потом строке, потом потом левая диагональ, правая диагональ или наоборот
     private static boolean isWin(char symb) {
-        int leftDiagonal = 0;
-        int rightDiagonal = 0;
-        while (true) {
-            for (int i = 0; i < SIZE - 1; i++) {
-                if (map[i][i] == map[i + 1][i + 1]) {
-                    leftDiagonal++;
+if (diagonal(symb) || vertHoriz(symb)) return true;
+return false;
+    }
+
+    public static boolean diagonal(char symb) {
+        boolean leftDiagonal, rightDiagonal;
+        leftDiagonal = true;
+        rightDiagonal = true;
+
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
+            rightDiagonal &= (map[i][i] == symb);
+            leftDiagonal &= (map[DOTS_TO_WIN - i - 1][i] == symb);
         }
+            if (leftDiagonal || rightDiagonal) return true;
+
+            return false;
+        }
+
+    public static boolean vertHoriz(char symb) {
+        boolean vert, horiz;
+        vert = true;
+        horiz = true;
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
+
+            for (int j = 0; j < DOTS_TO_WIN; j++) {
+
+                vert &= (map[i][j] == symb);
+                horiz &= (map[j][i] == symb);
+            }
+            if (vert || horiz) return true;
+        }
+            return false;
+        }
+
+
 //        if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
 //        if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
 //        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return true;
@@ -102,9 +132,6 @@ public class Cross {
 //        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return true;
 //        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
 //        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
-        return false;
-    }
-
 
     private static boolean isMapFull() {
         for (int i = 0; i < SIZE; i++) {
