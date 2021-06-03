901b5602e23d12c5a0280e2624303eed1eca695f
DmitriyDadinskiy <84646255+DmitriyDadinskiy@users.noreply.github.com>
2021-06-03 22:30:11 +1000

    /homeWork5

--- src/main/java/HomeWork5/Main.java
+++ src/main/java/HomeWork5/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Worker[] workArray = new Worker[5];
         workArray[0] = new Worker("Papikov Vpl", "Manager", "email@1", 99999999, 100, 41);
         workArray[1] = new Worker("Mamikov Mam", "secretary", "email@2", 88888888, 50, 51);
-        workArray[2] = new Worker("Karkov Dim", "engineer", "email@3", 77777777, 95, 42);
+        workArray[2] = new Worker("Karkov Dim", "engineer", "email@3", 7777777, 95, 42);
         workArray[3] = new Worker("Smorin Ra", "locksmith", "email@4", 666666666, 70, 43);
         workArray[2] = new Worker("Posledniy Paren", "trainee", "email@5", 555555555, 21, 54);
 
--- src/main/java/HomeWork5/Worker.java
+++ src/main/java/HomeWork5/Worker.java
@@ -1,2 +1,48 @@
-package HomeWork5;public class Worker {
+package HomeWork5;
+
+import java.util.StringJoiner;
+
+public class Worker {
+
+    //Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+
+    private String name;
+    private String position;
+    private String email;
+    private int phone;
+    private int cash;
+    private int age;
+
+    //Конструктор класса должен заполнять эти поля при создании объекта.
+
+    public Worker (String name, String position, String email, int phone,int cash, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.cash = cash;
+        this.age= age;
+    }
+
+    //Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
+
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                ", cash=" + cash +
+                ", age=" + age +
+                '}';
+    }
+
+    public void print(){
+    System.out.println(this);
+}
+    public int getAge (){
+        return age;
+    }
 }
